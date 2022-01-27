e201c2dd6f21c4af996c7e799dce7d95b45e2038
sharmarama4 <97950349+sharmarama4@users.noreply.github.com>
2022-01-27 23:05:29 +0100

    Add files via upload
    commited

---
+++ Multidimensen.java
@@ -0,0 +1,23 @@
+package arraysinjava;
+
+public class Arraysinjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	int[][] myNumbers= {{1,2,3,4},{5,6,7} };
+	int x = myNumbers[1][0];
+	System.out.println(x);
+		
+		
+		
+		
+		
+		
+		
+		
+	}	
+		
+
+}
+
